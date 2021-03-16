18abb728d1a85a229aec82b7d87778d0a1a85ce6
hieufix1710 <hieufix1710@gmail.com>
2021-03-16 14:13:05 +0700

    list-room

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,14 @@
 class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :customer
+  belongs_to :room
+
+  scope :rooms_booked, (lambda do |start_date, end_date|
+    get_room_ids
+    .where("(?)<=end_date and(?)>=start_date", start_date, end_date)
+  end)
+
+  scope :get_room_ids, ->{select(:room_id).not_delete}
+
+  scope :not_delete, ->{where(deleted: 0)}
 end
