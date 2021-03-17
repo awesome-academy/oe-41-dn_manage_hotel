0d71cf1ed21d21376e75ce5b52ba30f3d9b7781e
huyduong155 <nhathuy.155@gmail.com>
2021-03-17 14:51:07 +0700

    search rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,17 @@
 class RoomsController < ApplicationController
+  before_action :load_params, only: :index
+
   def index
-    params[:start_date] ||= Time.zone.today
-    params[:end_date] ||= Time.zone.today
-    @rooms = Room.rooms
+    rooms_booked = Booking.rooms_booked params[:start_date], params[:end_date]
+    @rooms = Room.rooms(rooms_booked)
                  .paginate(page: params[:page])
                  .per_page(Settings.paging_limit)
+  end
+
+  private
+
+  def load_params
+    params[:start_date] ||= Time.zone.today
+    params[:end_date] ||= Time.zone.today
   end
 end
