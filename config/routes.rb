5c873da9e434b131dcd344fbd68076372ed3ef99
huyduong155 <nhathuy.155@gmail.com>
2021-03-18 09:34:35 +0700

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root to: "static_pages#home"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    get "/logout", to: "sessions#destroy"
     resources :rooms, only: :index
     namespace :admin do
     end
